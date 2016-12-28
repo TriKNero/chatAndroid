43c3ce2bef7156a916a0a21ba8be7906af66f256
kristian <krisnero@yandex.ru>
2016-12-28 08:29:17 +0300

    chat app

---
+++ app/src/main/java/com/kristian/chatapp/ChatMessage.java
@@ -0,0 +1,36 @@
+package com.kristian.chatapp;
+
+import java.util.Date;
+
+/**
+ * Created by kristian on 24.12.16.
+ */
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage() {
+    }
+
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+}
